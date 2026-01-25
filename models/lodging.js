b60d88f2b13743ef86062a38d7a1c9b082238e6c
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-25 21:06:07 +0700

    migration()

---
+++ models/lodging.js
@@ -0,0 +1,30 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Lodging extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Lodging.init({
+    name: DataTypes.STRING,
+    facility: DataTypes.STRING,
+    roomCapacity: DataTypes.INTEGER,
+    imgUrl: DataTypes.STRING,
+    location: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    typeId: DataTypes.INTEGER,
+    authorId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Lodging',
+  });
+  return Lodging;
+};
