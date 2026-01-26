5b2721a01b69078f8a4b096957b5af788249dca6
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-26 23:34:34 +0700

    getRoomById()

--- controllers/LodgingController copy.js
+++ controllers/LodgingController copy.js
@@ -9,6 +9,18 @@ class LodgingController {
             res.status(500).json({ message: "Internal Server Error" });
         }
     }
+
+    static async getRoomById(req, res, next) {
+        try {
+            const { id } = req.params;
+            const room = await Lodging.findByPk(id);
+            if (!room) throw { name: "NotFound" };
+
+            res.status(200).json(room);
+        } catch (error) {
+            next(error);
+        }
+    }
 }
 
 module.exports = LodgingController;
