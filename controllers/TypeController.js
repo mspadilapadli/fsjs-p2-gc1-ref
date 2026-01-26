78c5737aeb739519ad3cd4e355944345c37f1139
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-26 23:30:35 +0700

    postType()

--- controllers/TypeController.js
+++ controllers/TypeController.js
@@ -1,5 +1,16 @@
 const { Type } = require("../models");
 class TypeController {
+    static async postType(req, res, next) {
+        try {
+            const type = await Type.create(req.body);
+            res.status(201).json({
+                message: `new type ${type.name} created `,
+                type,
+            });
+        } catch (error) {
+            next(error);
+        }
+    }
     static async getTypes(req, res, next) {
         try {
             const types = await Type.findAll();
