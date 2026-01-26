abef7c816d432af64bc3414505cc2775fd5408db
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-26 23:46:54 +0700

    routers types

--- routers/type.routes.js
+++ routers/type.routes.js
@@ -2,6 +2,12 @@ const TypeController = require("../controllers/typeController");
 
 const router = require("express").Router();
 
+router.post("/", TypeController.postType);
+
 router.get("/", TypeController.getTypes);
+
+router.put("/:id", TypeController.putType);
+
+router.delete("/:id", TypeController.deleteType);
 
 module.exports = router;
