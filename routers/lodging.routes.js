0fa3a77ac18cc9ff8ac82ea7855343dabd1174f4
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-01-27 23:51:41 +0700

    lodging routes

--- routers/lodging.routes.js
+++ routers/lodging.routes.js
@@ -1,6 +1,24 @@
 const LodgingController = require("../controllers/LodgingController copy");
-
 const router = require("express").Router();
 
 router.get("/", LodgingController.getLodgings);
+
+router.use(authentication);
+
+router.post("/", LodgingController.postRoom);
+
+router.get("/", LodgingController.getAllRoomsUser);
+
+router.get("/:id", LodgingController.getRoomById);
+
+router.put("/:id", authorization, LodgingController.putRoom);
+
+router.patch(
+    "/:id/imgUrl",
+    authorization,
+    upload.single("imgUrl"),
+    LodgingController.patchImgUrl,
+);
+
+router.delete("/:id", authorization, LodgingController.deleteRoom);
 module.exports = router;
