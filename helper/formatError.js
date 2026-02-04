de7947434285b68e12b492a5e2cc98b37c938fc2
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-04 23:20:19 +0700

    helper/formatError : add custome message for unique constrain

--- helper/formatError.js
+++ helper/formatError.js
@@ -1,9 +1,20 @@
-const formateSequelizeValidationError = (error) => {
+const UNIQUE_ERROR_MESSAGE = {
+    email: "Email already exists",
+    username: "Username already exists",
+};
+const formateSequelizeError = (error) => {
     const errors = {};
     error.errors.forEach((err) => {
-        errors[err.path] = err.message;
+        // unieque constraint error
+        if (error.name === "SequelizeUniqueConstraintError") {
+            errors[err.path] =
+                UNIQUE_ERROR_MESSAGE[err.path] || "Duplicate value";
+        } else {
+            // validation error
+            errors[err.path] = err.message;
+        }
     });
     return errors;
 };
 
-module.exports = formateSequelizeValidationError;
+module.exports = formateSequelizeError;
