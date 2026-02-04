56d914dc3b6541e974f647c3ce1eeb444325fe29
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-04 23:21:12 +0700

    middleware: errorHandler custome message for unique constrain

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -1,18 +1,16 @@
-const formateSequelizeValidationError = require("../helper/formatError");
+const formateSequelizeError = require("../helper/formatError");
 
 const errorHandler = (error, req, res, next) => {
     let code = error.name || "INTERNAL_SERVER_ERROR";
     let message = error.message || "Internal Server Error";
     let status = error.status || 500;
 
-    if (error.name === "SequelizeValidationError") {
+    if (
+        error.name === "SequelizeValidationError" ||
+        error.name === "SequelizeUniqueConstraintError"
+    ) {
         code = "BAD_REQUEST";
-        message = formateSequelizeValidationError(error);
-        status = 400;
-    }
-    if (error.name === "SequelizeUniqueConstraintError") {
-        code = "BAD_REQUEST";
-        message = error.errors.map((err) => err.message);
+        message = formateSequelizeError(error);
         status = 400;
     }
 
