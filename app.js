6afed9781b076e8003c0398443ab32ff38bbd844
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-04 22:32:12 +0700

    app use errorHandler middleware

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const port = 3000;
 app.use(express.urlencoded({ extended: true })); // Middleware to parse URL-encoded bodies
 app.use(express.json()); // Middleware to parse JSON bodies
 app.use("/", require("./routers")); // Importing routes from routes/index.js
+app.use(require("./middleware/errorHandler")); // Error handling middleware
 
 app.listen(port, () => {
     console.log(`App listening at http://localhost:${port}`);
