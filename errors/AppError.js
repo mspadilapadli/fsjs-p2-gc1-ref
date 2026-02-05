58b0e7a3aff14af10893da81276ec4f29b5e987c
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-05 09:29:40 +0700

    middleware: add params error to AppError class

--- errors/AppError.js
+++ errors/AppError.js
@@ -1,8 +1,9 @@
 class AppError extends Error {
-    constructor(name, message, status) {
+    constructor(name, message, status, errors = null) {
         super(message);
         this.name = name;
         this.status = status;
+        this.errors = errors;
 
         Error.captureStackTrace(this, this.constructor);
     }
