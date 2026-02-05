c6759a88185e8f5146598a615bb1ef458f85ebb3
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-05 09:31:04 +0700

    middleware: adjust response error

--- middleware/errorHandler.js
+++ middleware/errorHandler.js
@@ -4,17 +4,24 @@ const errorHandler = (error, req, res, next) => {
     let code = error.name || "INTERNAL_SERVER_ERROR";
     let message = error.message || "Internal Server Error";
     let status = error.status || 500;
+    let errors;
 
     if (
         error.name === "SequelizeValidationError" ||
         error.name === "SequelizeUniqueConstraintError"
     ) {
         code = "BAD_REQUEST";
-        message = formateSequelizeError(error);
+        message = "Validation Error";
+        errors = formateSequelizeError(error);
         status = 400;
     }
 
-    res.status(status).json({ code, message });
+    const response = { code, message };
+    if (errors) {
+        response.errors = errors;
+    }
+
+    res.status(status).json(response);
 };
 
 module.exports = errorHandler;
