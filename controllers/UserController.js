69e6c047da63f51f9515168b4e0674669978f292
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-05 09:28:50 +0700

    ctrl : user login

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,4 +1,6 @@
 const AppError = require("../errors/AppError");
+const { comparePassword } = require("../helper/bcrypt");
+const { createToken } = require("../helper/jwt");
 const { User } = require("../models");
 class UserController {
     static async register(req, res, next) {
@@ -23,12 +25,26 @@ class UserController {
             const { email, password } = req.body;
             if (!email || !password)
                 throw new AppError(
-                    "InvalidInput",
+                    "INVALID_INPUT",
                     "Email and password are required",
                     400,
                 );
+
+            const user = await User.findOne({ where: { email } });
+            if (!user) throw new AppError("INVALID_USER", "Invalid email", 401);
+
+            const isPasswordValid = await comparePassword(
+                password,
+                user.password,
+            );
+            if (!isPasswordValid)
+                throw new AppError("INVALID_USER", "Invalid password", 401);
+
+            const access_token = createToken({
+                id: user.id,
+            });
+            res.status(200).json({ access_token });
         } catch (error) {
-            console.log(error);
             next(error);
         }
     }
