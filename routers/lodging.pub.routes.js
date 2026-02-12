5054aa1e510b9f69ae6295eca2987d3d7b3ce0cd
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-12 23:52:44 +0700

    public routers loging

--- routers/lodging.pub.routes.js
+++ routers/lodging.pub.routes.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+const LodgingController = require("../controllers/LodgingController");
 
 router.get("/lodgings", LodgingController.getAllRooms); //public
 router.get("/lodgings/:id", LodgingController.getRoomById); //public
