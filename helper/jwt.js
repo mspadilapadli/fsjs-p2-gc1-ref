38b3dafceade239bb50eb6af5be374bec8cce92c
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-12 23:54:24 +0700

    jwt with secret

--- helper/jwt.js
+++ helper/jwt.js
@@ -1,5 +1,5 @@
 const jwt = require("jsonwebtoken");
-// const secret = process.env.JWT_SECRET;
+const secret = process.env.JWT_SECRET;
 
 const createToken = (payload) => {
     const token = jwt.sign(payload, "secret");
