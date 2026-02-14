525dc7da0de06b7cf4707b07e5b21afaef409ce5
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2026-02-14 23:55:19 +0700

    user routes

--- routers/user.routes.js
+++ routers/user.routes.js
@@ -1,12 +1,10 @@
 const router = require("express").Router();
 const UserController = require("../controllers/UserController");
-const { adminAuthorize } = require("../middlewares/authorization");
-const authentication = require("../middlewares/authenticate");
 
 router.post(
     "/add-user",
-    authentication,
-    adminAuthorize,
+    // authentication,
+    // adminAuthorize,
     UserController.register,
 );
 
